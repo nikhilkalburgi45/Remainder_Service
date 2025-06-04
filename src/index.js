b34228be5cb9ed1edd4779d28267a65989efe0b6
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-06-04 11:31:49 +0530

    Refactor ticket creation to use controller and integrate message queue channel setup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+const { subscribeMessage, createChannel } = require("./utils/messageQueue");
+
 // const { sendBasicEmail } = require("./services/email-service");
 const { setupJobs } = require("./utils/job");
-const { create } = require("./controllers/ticket-controllers");
+const ticketController = require("./controllers/ticket-controllers");
 
 const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.post("/api/v1/tickets", create);
+  const channel = await createChannel();
+
+  app.post("/api/v1/tickets", ticketController.create);
 
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 
-    setupJobs();
+    // setupJobs();
   });
 };
 
